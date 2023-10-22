13de63296a7571e587fdf1dbc3e33469fbe7d1c9
Suni <147529813+galaxysuni@users.noreply.github.com>
2023-10-22 19:11:31 -0300

    Add files via upload

---
+++ Sprint - Yamila Awad/Javascript/script.js
@@ -0,0 +1,18 @@
+let edad = 18;
+if (edad >=18) {
+    console.log ("sos mayor de edad");
+}
+else {
+    console.log ("sos menor de edad";)
+}
+
+let nota = 8;
+if (nota >=9) {
+    console.log ("Promocionado");
+} else if (nota >=6) {
+    console.log ("Aprobado");
+} else {
+    console.log ("Insuficiente");
+}
+
+alert ("Bienvenidx a mi página :)");
